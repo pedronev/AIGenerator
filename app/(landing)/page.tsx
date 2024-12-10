ed2fd64e0f57f7edbeacb59726926ace1ffd7379
pedro nevarez <pedro_nevarez_tz@hotmail.com>
2024-12-10 13:51:10 -0600

    initial commit

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <p>Landing page</p>
+        </div>
+    )
+}
+
+export default LandingPage;
